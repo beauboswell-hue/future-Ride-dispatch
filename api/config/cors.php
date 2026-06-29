3a6cf07319c635055dc2f8f70f1423c794f88195
beauboswell-hue <beauboswell@gmail.com>
2026-06-29 10:46:33 +0000

    config: save cloud upload and storage fixes

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(['http://localhost:4200', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST')), ...Utils::arrayFrom(env('FRONTEND_HOSTS', ''))]),
+    'allowed_origins' => array_filter([
+        'http://localhost:4200',
+        'https://future.limo',
+        'https://www.future.limo',
+        env('CONSOLE_HOST'),
+        Utils::addWwwToUrl(env('CONSOLE_HOST')),
+        ...Utils::arrayFrom(env('FRONTEND_HOSTS', ''))
+    ]),
 
     'allowed_origins_patterns' => [],
 
